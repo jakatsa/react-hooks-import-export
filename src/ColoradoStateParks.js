1ebda38e33a3eab876836594ceabd1b7dfe3c7d4
Joe <jakatsa5@gmail.com>
2024-02-01 15:08:05 +0300

    completed assignment

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,17 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
-
+import MesaVerde from "./parks/MesaVerde";
+import { trees, wildlife, elevation } from "./parks/RockyMountain";
 function ColoradoStateParks() {
-  howManyParks(); // => "42 parks!"
-
-  return <h1>Colorado State Parks!</h1>;
+  howManyParks();
+  elevation();
+  wildlife();
+  return (
+    <>
+      <h1>Colorado State Parks!</h1>
+      <MesaVerde />
+      <p>{trees}</p>
+    </>
+  );
 }
+export default ColoradoStateParks;
